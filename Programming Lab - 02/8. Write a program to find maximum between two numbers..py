1bd50123175042bce42108ac102ee652b81a41b6
Bhupinder Kumar <97667019+bhupi080@users.noreply.github.com>
2022-09-06 23:09:48 +0530

    Update 8. Write a program to find maximum between two numbers..py

---
+++ Programming Lab - 02/8. Write a program to find maximum between two numbers..py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number"))
+
+if num1 > num2:
+    print(num1,"is greater")
+elif num1 < num2:
+    print(num2,"is greater")
+else:
+    print("Both are equal")
